8fa81f94a43d70564fa3db62eb8f7731bff7d277
lonely43 <iladenisov327@gmail.com>
2024-06-08 17:15:01 +0300

    links

--- !public/nav/nav.js
+++ !public/nav/nav.js
@@ -1,5 +1,5 @@
-let btn = document.querySelector(".brgBtn")
-let list = document.querySelector(".list")
+let btn = document.querySelector("nav .brgBtn")
+let list = document.querySelector("nav .list")
 btn.addEventListener('click', ()=>{
    list.classList.toggle('show')
 }, false)
